5c8bbe3ffc8cc83851b11a74e6826f7b6102ffaf
kromskii2 <kromskii2official@gmail.com>
2025-01-28 23:53:12 +0700

    Удалены ненужные файлы и добавлены новые скрипты для развертывания и тестирования проекта

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+from src.main import clean_temp_folder
+
+class TestCleanTempFolder(unittest.TestCase):
+
+    def test_clean_temp_folder(self):
+        # Здесь можно добавить тесты для проверки работы функции clean_temp_folder
+        # Например, можно использовать mock для проверки удаления файлов
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
